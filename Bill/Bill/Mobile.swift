57ca759726ca2bda695ccc9148ff5f28b0f06249
Adithya Sai Neeli <neeliadithyasai@gmail.com>
2020-02-13 17:59:21 -0500

    [AN] created mobile class

---
+++ Bill/Bill/Mobile.swift
@@ -0,0 +1,13 @@
+//
+//  Mobile.swift
+//  C0771910_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Mobile
+{
+    
+}
