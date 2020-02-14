3afbda519a015c5f5e01e98987a32e3c418ea8d9
Adithyasai Neeli <neeliadithyasai@gmail.com>
2020-02-14 17:25:49 -0500

    [AN] Created customer object for testing

--- Bill/Bill/main.swift
+++ Bill/Bill/main.swift
@@ -38,3 +38,6 @@ var H1 = Hydro(Bill_Id: 01, Bill_Date: date3, Bill_Type: BILL_TYPE.HYDRO, Total_
 
     H1.display()
 
+var C1 =  Customer(Customer_Id: 01, Email_Id: "neeliadithyasai@gmail.com")
+
+    C1.display()
