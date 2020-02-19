40b2e27dead4f055587b200d7531b463ec10e1ad
Adithyasai Neeli <neeliadithyasai@gmail.com>
2020-02-18 19:00:02 -0500

    [AN] Added init methods in insurance class

--- Bill/C0771910_W2020_MID_TERM_MAD3004/Insurance.swift
+++ Bill/C0771910_W2020_MID_TERM_MAD3004/Insurance.swift
@@ -13,12 +13,25 @@ enum Insurance_Type
 }
 class Insurance: Bill
 {
+  
+    
        var insurance_Provider = String()
        var insurance_Type: Insurance_Type?
        var insurance_StartDate: Date = Date()
        var insurance_EndDate: Date = Date()
        var total_Days: Int = 0
        var total_Installment: Double = 0.0
+    
+    
+        init( Bill_Id: String, Bill_Date: Date, Bill_Type: BILL_TYPE, Total_Bill_Amount: Double,insurance_Provider: String, insurance_Type: Insurance_Type, insurance_StartDate: Date, insurance_EndDate: Date, total_Installment: Double) {
+         
+          super.init(Bill_Id: Bill_Id, Bill_Date: Bill_Date, Bill_Type: Bill_Type, Total_Bill_Amount: Total_Bill_Amount)
+          self.insurance_Provider = insurance_Provider
+          self.insurance_Type = insurance_Type
+          self.insurance_StartDate = insurance_StartDate
+          self.insurance_EndDate = insurance_EndDate
+          self.total_Installment = total_Installment
+      }
     
     
 }
